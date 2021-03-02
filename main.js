23ccdca52d2c6092ebac309cb55763596ac80755
ManerNiyet <manernieyt15@gmail.com>
2021-03-02 13:18:28 -0500

    created html and js files

---
+++ main.js
@@ -0,0 +1,6 @@
+var numberOfKids = 2;
+var spouse = 'Sponge Bob';
+var city = 'Underwater World';
+var jobTitle = 'mermaid';
+var future = 'You will be a ' + jobTitle + ' in ' + city + ' married to ' + spouse + ' with ' + numberOfKids + ' kids. ';
+console.log(future);
